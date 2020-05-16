d090505bf5f8e934697bf33e679ae06381485b0d
Manny <egalli64@gmail.com>
2020-05-16 11:54:15 +0200

    Using DataSource

---
+++ src/main/java/dd/mhja/srv/Hello.java
@@ -0,0 +1,47 @@
+package dd.mhja.srv;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dd.mhja.dao.HibUtil;
+
+@WebServlet("/hello")
+public class Hello extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static final Logger LOG = LoggerFactory.getLogger(Hello.class);
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        LOG.trace("enter");
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+
+        try (PrintWriter writer = response.getWriter()) {
+            try (Session session = HibUtil.getSessionFactory().openSession()) {
+                if(session.isConnected()) {
+                    writer.println("Hello!");       
+                } else {
+                    writer.println("No connection to database");       
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
