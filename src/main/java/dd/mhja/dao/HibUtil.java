32674762a9c6f3f310b64a3f458ea420c0f50cdd
Manny <egalli64@gmail.com>
2020-05-18 09:05:34 +0200

    dropped session support

--- src/main/java/dd/mhja/dao/HibUtil.java
+++ src/main/java/dd/mhja/dao/HibUtil.java
@@ -1,40 +1,12 @@
 package dd.mhja.dao;
 
-import java.util.Properties;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
-import org.hibernate.service.ServiceRegistry;
-
 public class HibUtil {
-    private static SessionFactory sessionFactory;
     private static EntityManagerFactory factory = Persistence.createEntityManagerFactory("me");;
-    
-    static {
-        Configuration configuration = new Configuration();
-        Properties settings = new Properties();
 
-        settings.put(Environment.DATASOURCE, "java:comp/env/jdbc/me");
-        settings.put(Environment.SHOW_SQL, "true");
-        configuration.setProperties(settings);
-
-        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                .applySettings(configuration.getProperties()).build();
-
-        sessionFactory = configuration.buildSessionFactory(serviceRegistry);        
-    }
-
-    public static Session getSession() {
-        return sessionFactory.openSession();
-    }
-    
     public static EntityManager getEntityManager() {
         return factory.createEntityManager();
     }
