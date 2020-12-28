627feb4725e64fefbefabedf3c7d718eafe670c6
Manny <egalli64@gmail.com>
2020-12-28 11:35:16 +0100

    explicit query

--- src/main/java/com/example/jed/s06/CoderDao.java
+++ src/main/java/com/example/jed/s06/CoderDao.java
@@ -3,13 +3,15 @@ package com.example.jed.s06;
 import java.util.List;
 
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 import com.example.jed.s05.Coder05;
 
 public class CoderDao {
     public List<Coder05> getAll() {
         try (Session session = HibernateUtil.getSession()) {
-            return session.createQuery("from Coder05", Coder05.class).list();
+            Query<Coder05> query = session.createQuery("from Coder05", Coder05.class);
+            return query.list();
         }
     }
 }
