e4fd0bbd279d42673bcfa0e04ee035081ae3c5ca
Manny <egalli64@gmail.com>
2021-01-03 13:54:26 +0100

    final class

--- src/main/java/com/example/jed/s08/JpaUtil.java
+++ src/main/java/com/example/jed/s08/JpaUtil.java
@@ -4,7 +4,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public abstract class JpaUtil {
+public final class JpaUtil {
+    private JpaUtil() {
+    }
+
     private static EntityManagerFactory factory = Persistence.createEntityManagerFactory("me");
 
     public static EntityManager createEntityManager() {
