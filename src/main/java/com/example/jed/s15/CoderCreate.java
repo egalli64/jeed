71f44a7980821ac1a6b64fca34b6ce521ed80083
Manny <egalli64@gmail.com>
2021-01-03 19:30:02 +0100

    generated value

---
+++ src/main/java/com/example/jed/s15/CoderCreate.java
@@ -0,0 +1,40 @@
+package com.example.jed.s15;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebServlet("/s15/coder/create")
+public class CoderCreate extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static final Logger log = LoggerFactory.getLogger(CoderCreate.class);
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        log.trace("enter");
+
+        Coder15 coder = new Coder15("Frank", "Fripp", 1500.0);
+        if (new CoderDao().create(coder)) {
+            log.debug("Coder persisted with id " + coder.getId());
+            request.setAttribute("coder", coder);
+        } else {
+            log.info("Can't create " + coder);
+        }
+
+        request.getRequestDispatcher("/coder.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
