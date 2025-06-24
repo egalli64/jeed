8f74986ed46d6eff4fc2fae4e2dcec21bd160864
Emanuele <egalli64@gmail.com>
2025-06-24 15:49:18 +0200

    step two

--- src/main/java/com/example/jeed/svc/EmployeeCarService.java
+++ src/main/java/com/example/jeed/svc/EmployeeCarService.java
@@ -5,21 +5,59 @@
  */
 package com.example.jeed.svc;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.example.jeed.dao.Car;
 import com.example.jeed.dao.CarDao;
 import com.example.jeed.dao.Employee4Car;
 import com.example.jeed.dao.Employee4CarDao;
+import com.example.jeed.util.EntityManagerService;
 
-import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 
 public class EmployeeCarService {
+    private static final Logger log = LogManager.getLogger(EmployeeCarService.class);
 
-    /**
-     * Constructor - inject the required dependencies
-     */
-    public EmployeeCarService(Employee4CarDao employeeDao, CarDao carDao, EntityManagerFactory emf) {
+    private CarDao carDao;
+    private Employee4CarDao employeeDao;
+    private EntityManagerService entityManagerService;
+
+    public EmployeeCarService(CarDao carDao, Employee4CarDao employeeDao, EntityManagerService entityManagerService) {
+        this.carDao = carDao;
+        this.employeeDao = employeeDao;
+        this.entityManagerService = entityManagerService;
     }
 
-    public void create(Car car, Employee4Car employee) {
+    public void create(Employee4Car employee, Car car) {
+        log.traceEntry();
+
+        EntityTransaction transaction = null;
+
+        try (EntityManager em = entityManagerService.createEntityManager()) {
+            carDao.setEm(em);
+            employeeDao.setEm(em);
+
+            transaction = em.getTransaction();
+            transaction.begin();
+
+            car.setEmployee(employee);
+            employee.setCar(car);
+
+            employeeDao.create(employee);
+            carDao.create(car);
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+                log.debug("Rollback: {}", e.getMessage());
+            }
+            throw new RuntimeException("Failure creating employee + car", e);
+        } finally {
+            carDao.removeEm();
+            employeeDao.removeEm();
+        }
     }
 }
